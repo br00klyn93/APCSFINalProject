300d7786d929b6730e8310480feafab36800dd20
br00klyn93 <bmclaury93@gmail.com>
2019-05-28 20:34:15 -0700

    Fixes

--- static/main.js
+++ static/main.js
@@ -1,21 +1,41 @@
 function shake() {
   var el = document.getElementById("main-img");
+  var box = document.getElementById("box");
+  var submit = document.getElementById("submit");
+
   el.classList.add("animated");
   el.classList.add("bounceIn");
 
   setTimeout(function () {
     el.classList.remove("animated");
     el.classList.remove("bounceIn");
-  }, 500);
+    el.style.display = "none";
+  }, 1000);
+
+  box.style.backgroundColor = "#06A10B";
+  submit.style.display = "block";
+}
+
+function finalSubmit() {
+  var box = document.getElementById("box");
+
+  box.classList.remove("animated");
+  box.classList.remove("fadeInLeft");
+  box.classList.remove("delay-1s");
+
+  setTimeout(function () {
+    box.classList.add("animated");
+    box.classList.add("bounceIn");
+  }, 200);
+
+
 }
 
 function delays() {
-  console.log("Work");
   var rows = document.getElementsByTagName("li");
   var place = 1;
   var i;
   for(i = 0; i < rows.length; i++) {
-    console.log("ran");
     var time = "delay-"+place+"s";
     rows[i].classList.add("animated");
     rows[i].classList.add("fadeInUp");
