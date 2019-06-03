9a20e7fb668c23e5c60ce1b271b8f299d9d9588a
br00klyn93 <bmclaury93@gmail.com>
2019-06-03 11:08:26 -0700

    Nikhil Fix

--- GrammarChecker.py
+++ GrammarChecker.py
@@ -10,17 +10,14 @@ from selenium.webdriver.common.keys import Keys
 # imports time for getting the program to sleep while the grammarly does its work
 import time
 # uses the chrome driver to navigate grammarly
-driver = webdriver.Chrome("chromedriver.exe")
+import pickle
+
 
 # function for logging in requires username and password
 
 
-def login(username, password):
+def login(driver, username, password):
 
-    # timeouts if grammarly takes mre than 10 seconds for the sake of server
-    driver.set_page_load_timeout("10")
-    # instrcts chrome to go to the signin page
-    driver.get("https://www.grammarly.com/signin?allowUtmParams=true")
     driver.find_element_by_class_name(
         "_2IV5P-_-_-client-guidelines-input_field--input_field-input").send_keys(username)  # Finds the username text box and sends the username
     time.sleep(0.5)
@@ -30,7 +27,7 @@ def login(username, password):
 # checks the document
 
 
-def CheckDocument(document):
+def CheckDocument(driver, document):
     driver.find_element_by_class_name(
         "_758e07ad-document_item-add").click()  # finds the add new doc button
     time.sleep(3)
@@ -39,5 +36,9 @@ def CheckDocument(document):
     time.sleep(6.5)
     driver.find_element_by_xpath(
         "//*[@id=\"page\"]/div/div/div/div/div[2]/div[1]/div[8]/div[2]/div/div[2]/div/div[2]/div[1]/div/div/div/div/div[1]").click()  # finds the view all alerts button to close the "get premium tab"
-    time.sleep(0.5)
-    driver.close()  # closes chrome
+
+
+def PostUrlCookies(driver):
+    cookie = str(driver.get_cookies())
+    print(cookie)
+    print(driver.current_url)
