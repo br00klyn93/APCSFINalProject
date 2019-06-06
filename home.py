30dce40ac587e378e03767eded776903b05a484d
br00klyn93 <bmclaury93@gmail.com>
2019-06-06 10:08:35 -0700

    Update home.py

--- home.py
+++ home.py
@@ -30,9 +30,11 @@ def gcheck():
     app_id = "60edc74a"
     app_key = "6b8466506d884d62a86f1e2d283f2286"
     language = "en-gb"
-    word_id = "example"
+    word_id = word
     url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
     r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
+    
+    result = r.text
     
     return r.text
 
